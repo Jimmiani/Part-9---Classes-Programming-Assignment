fa2eed84b021123538170b8acd2e23642316d223
Emmett Cornies <339761330@GoTVDSB.ca>
2025-08-21 02:02:27 -0400

    Add project files.

---
+++ Part 9 - Classes Programming Assignment/Program.cs
@@ -0,0 +1,209 @@
+﻿using System.Transactions;
+using System.Xml.Serialization;
+
+namespace Part_9___Classes_Programming_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool done = false;
+            string choice = "", fName, lName;
+            int choice2 = 0;
+            List <Student> students = new List <Student>();
+            Console.SetBufferSize(200, 200);
+            Console.SetWindowSize(150, 40);
+
+            students.Add(new Student("Emmett", "Cornies"));
+            students.Add(new Student("Fred", "Flintstone"));
+            students.Add(new Student("Lightning", "McQueen"));
+            students.Add(new Student("Jack", "Sparrow"));
+            students.Add(new Student("Tony", "Stark"));
+
+            while (!done)
+            {
+                // Intro
+
+                Console.SetCursorPosition(35, 2);
+                Console.WriteLine("Welcome to my extremely cool menu screen for my classes programming assignment!");
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("Pick what part of the assignment you'd like to see.");
+                Console.WriteLine("Option 1: Display Students");
+                Console.WriteLine("Option 2: Student Details");
+                Console.WriteLine("Option 3: Add a Student");
+                Console.WriteLine("Option 4: Remove a student");
+                Console.WriteLine("Option 5: ...");
+                Console.WriteLine("Or, you can press 'Q' to exit the program.");
+                Console.WriteLine();
+                Console.Write("Pick your option (1 - ..., or 'Q'.): ");
+                choice = Console.ReadLine();
+                while (choice != "1" && choice != "2" && choice != "3" && choice != "4")
+                {
+                    Console.Write("Invalid Input. Try again: ");
+                    choice = Console.ReadLine();
+                }
+
+                // Display Students
+
+                if (choice == "1")
+                {
+                    Console.Clear();
+                    Console.WriteLine("Here's a list of all the students in the class:");
+                    Console.WriteLine();
+                    DisplayStudents(students);
+                    Console.WriteLine();
+                    Console.WriteLine("Press 'ENTER' to return to the main menu.");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+
+                // Student Details
+
+                else if (choice == "2")
+                {
+                    while (true)
+                    {   
+                        Console.Clear();
+                        Console.WriteLine("Here's a list of all the students in the class:");
+                        Console.WriteLine();
+                        StudentDetails(students, choice2);
+                        Console.WriteLine();
+                        Console.WriteLine("Press 'Q' to return to the main menu, or press 'R' to view another student's details.");
+                        Console.Write("Enter your choice here: ");
+                        choice = Console.ReadLine();
+                        choice = choice.ToUpper().Trim();
+                        while (choice != "Q" && choice != "R")
+                        {
+                            Console.Write("Invalid Input. Try again: ");
+                            choice = Console.ReadLine();
+                            choice = choice.ToUpper().Trim();
+                        }
+                        if (choice == "Q")
+                        {
+                            break;
+                        }
+                        else if (choice == "R")
+                        {
+                            continue;
+                        }
+                    }
+                    Console.Clear();
+                }
+
+                // Add a Student
+
+                else if (choice == "3")
+                {
+                    while (true)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Here's a list of all the students in the class:");
+                        Console.WriteLine();
+                        DisplayStudents(students);
+                        Console.WriteLine();
+                        Console.WriteLine("Enter in the name of the student you'd like to add.");
+                        Console.WriteLine();
+                        Console.Write("First name: ");
+                        fName = Console.ReadLine();
+                        Console.Write("Last name: ");
+                        lName = Console.ReadLine();
+                        students.Add(new Student(fName, lName));
+                        Console.WriteLine();
+                        Console.WriteLine($"{students[students.Count - 1]} has been added to the class.");
+                        Console.WriteLine("Press 'Q' to return to the main menu, or press 'R' to add another student to the class.");
+                        Console.Write("Enter your choice here: ");
+                        choice = Console.ReadLine();
+                        choice = choice.ToUpper().Trim();
+                        while (choice != "Q" && choice != "R")
+                        {
+                            Console.Write("Invalid Input. Try again: ");
+                            choice = Console.ReadLine();
+                            choice = choice.ToUpper().Trim();
+                        }
+                        if (choice == "Q")
+                        {
+                            break;
+                        }
+                        else if (choice == "R")
+                        {
+                            continue;
+                        }
+                    }
+                    Console.Clear();
+                }
+
+                // Remove a Student
+
+                else if (choice == "4")
+                {
+                    while (true)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("Here's a list of all the students in the class:");
+                        Console.WriteLine();
+                        for (int i = 0; i < students.Count; i++)
+                        {
+                            Console.WriteLine($"({i}) {students[i]}");
+                        }
+                        Console.WriteLine();
+                        Console.Write("Enter in the corresponding number of the student who you'd like to remove: ");
+                        while (!int.TryParse(Console.ReadLine(), out choice2) || choice2 >= students.Count)
+                        {
+                            Console.Write("Invalid Input. Try again: ");
+                        }
+                        Console.WriteLine();
+                        Console.WriteLine($"{students[choice2]} has been removed from the class.");
+                        students.RemoveAt(choice2);
+                        Console.WriteLine();
+                        Console.WriteLine("Press 'Q' to return to the main menu, or press 'R' to remove another student from the class.");
+                        Console.Write("Enter your choice here: ");
+                        choice = Console.ReadLine();
+                        choice = choice.ToUpper().Trim();
+                        while (choice != "Q" && choice != "R")
+                        {
+                            Console.Write("Invalid Input. Try again: ");
+                            choice = Console.ReadLine();
+                            choice = choice.ToUpper().Trim();
+                        }
+                        if (choice == "Q")
+                        {
+                            break;
+                        }
+                        else if (choice == "R")
+                        {
+                            continue;
+                        }
+                    }
+                    Console.Clear();
+                }
+            }
+        }
+        public static void DisplayStudents(List<Student> students)
+        {
+            for (int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine(students[i]);
+            }
+        }
+
+        public static void StudentDetails(List<Student> students, int choice)
+        {
+            for (int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine($"({i}) { students[i]}");
+            }
+            Console.WriteLine();
+            Console.Write("Enter in the corresponding number of the student whose details you'd like to view: ");
+            while (!int.TryParse(Console.ReadLine(), out choice) || choice >= students.Count)
+            {
+                Console.Write("Invalid Input. Try again: ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"First name: {students[choice].FirstName}");
+            Console.WriteLine($"Last name: {students[choice].LastName}");
+            Console.WriteLine($"Student number: {students[choice].StudentNumber}");
+            Console.WriteLine($"Email address: {students[choice].Email}");
+        }
+    }
+}
---
+++ Part 9 - Classes Programming Assignment/Student.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_9___Classes_Programming_Assignment
+{
+    internal class Student
+    {
+        private static Random generator = new Random();
+        private string _firstName;
+        private string _lastName;
+        private int _studentNumber;
+        private string _email;
+        private int _fNameEmail, _lNameEmail;
+
+        public Student(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _studentNumber = generator.Next(0, 1000) + 555000;
+            GenerateEmail();
+        }
+
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; GenerateEmail(); }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; GenerateEmail(); }
+        }
+
+        public int StudentNumber
+        {
+            get { return _studentNumber; }
+        }
+
+        public override string ToString()
+        {
+            return _firstName + " " + _lastName;
+        }
+
+        public void ResetStudentNumber(int oldNum)
+        {
+            _studentNumber = generator.Next(0, 1000) + 555000;
+            while (_studentNumber == oldNum)
+            {
+                _studentNumber = generator.Next(0, 1000) + 555000;
+            }
+            GenerateEmail();
+        }
+        public string Email
+        {
+            get { return _email; }
+        }
+
+        private void GenerateEmail()
+        {
+            for (int i = 0; i <= _firstName.Length; i++)
+            {
+                _fNameEmail = i;
+                if (_fNameEmail >= 3)
+                {
+                    break;
+                }
+            }
+            for (int i = 0; i <= _lastName.Length; i++)
+            {
+                _lNameEmail = i;
+                if (_lNameEmail >= 3)
+                {
+                    break;
+                }
+            }
+            _email = _firstName.Substring(0, _fNameEmail) + _lastName.Substring(0, _lNameEmail) + _studentNumber.ToString().Substring(_studentNumber.ToString().Length - 3, 3) + "@ICS4U.com";
+        }
+    }
+}
